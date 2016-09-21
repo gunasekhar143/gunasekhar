c99533a47e5f4b3f8443d607c3a0c8d0ce09ab99
gunasekhar143 <gunasekhar9ac@gmail.com>
2016-09-21 14:34:51 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Hello
+{
+public static void main(String []args)
+{
+	
+String str="Hello";
+String s=str.substring(4);
+System.out.println(s);
+System.out.println(s+str);
+System.out.println(s+str+s);
+}
+}
